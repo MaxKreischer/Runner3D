e129b1fa2dcb0b2aae187bf3c86aa24fd59ae183
Fruitlobster <altenburg.florian@web.de>
2018-04-29 19:39:59 +0200

    I changed the camera. I hope you like it. :)

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -12,9 +12,14 @@ public class CameraManager : MonoBehaviour {
     private Quaternion InitialCamRot;
     private Vector3 RotAngles;
     private Vector3 upDir, forwDir;
+    private Vector3 mouseMovement;
+    private Vector3 camOrigin;
+    private float circleValueXZ;
 
     public void InitCam(ref Transform playerTf)
     {
+        mouseMovement = new Vector3(0, 0, 0);
+        circleValueXZ = 0;
         transform.position = playerTf.position + new Vector3(0f, CameraUpShift, -CameraBackShift);
         Vector3 toTarget = playerTf.position - transform.position;
         Vector3 toTargetXRot = new Vector3(0f, toTarget.y, toTarget.z);
@@ -27,17 +32,33 @@ public class CameraManager : MonoBehaviour {
         //float zAngle = Vector3.Angle(transform.right, toTargetZRot);
         transform.rotation = Quaternion.Euler(25f, 0f, 0f);
         InitialCamRot = transform.rotation;
+        camOrigin = transform.position;
+
     }
 
 
     public void updateTransform(ref Transform playerTf, ref InputManager iM)
     {
-        transform.position = new Vector3(Mathf.Lerp(transform.position.x, playerTf.position.x, Time.deltaTime * 10f),
+        //Move camera such that its position --relative to the player-- stays the same
+        camOrigin = new Vector3(Mathf.Lerp(transform.position.x, playerTf.position.x, Time.deltaTime * 10f),
                                             transform.position.y, 
-                                            Mathf.Lerp(transform.position.z, playerTf.position.z, Time.deltaTime * 10f) + zOffset);
+                                            Mathf.Lerp(transform.position.z, playerTf.position.z, Time.deltaTime * 10f) );
 
 
-        
+        //Movement of the camera around the player
+        //Imagine a circle around the player. When we move the mouse to the left, we move the camera on the circle anti-clockwise. If we move the mouse to the right, we move the camera on the circle clockwise.
+        mouseMovement = iM.mouseMovement();
+        circleValueXZ -= mouseMovement.x;
+        circleValueXZ = circleValueXZ % 360f;
+
+        transform.position = new Vector3(camOrigin.x + Mathf.Sin(circleValueXZ * Mathf.Deg2Rad), camOrigin.y, camOrigin.z + Mathf.Cos(-circleValueXZ * Mathf.Deg2Rad) * zOffset);
+
+        //Rotate the camera. It is a little bit fake, because I do not calculate to rotate the camera to look at the player, but rather just use the "circle movement" from the mouse as above
+        transform.rotation = Quaternion.Euler(25f,-circleValueXZ,0);
+
+
+        //Rotate camera through mouse control resp. analog stick from the controller
+        /*
         forwDir = iM.MousePosWorld;
         //upDir = Vector3.;
         RotAngles = Quaternion.LookRotation(iM.MouseRay.direction).eulerAngles;
@@ -55,6 +76,6 @@ public class CameraManager : MonoBehaviour {
 
 
         //transform.rotation = Quaternion.Euler(iM.mouse_y, iM.mouse_x,0f);
-        
+        */
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -15,25 +15,29 @@ public class InputManager : MonoBehaviour {
     public float[] Mouse_Y_Arr = new float[mouseBufferSize];
     public float mouse_y = 0;
     public bool jumpBtn;
-    public Vector3 MousePosWorld;
+    private Vector3 MousePosWorld;
     public Ray MouseRay;
     private Vector3 mousePos;
+    private Vector3 lastMousePos = new Vector3(0,0,0);
 
     public float Forward { get { return this._forward; }}
 
     public void updateInputs(ref Camera cam)
     {
-        
-        _forward     = Input.GetAxis("Vertical");
-        horizontal  = Input.GetAxis("Horizontal");
+
+        _forward = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
         jumpBtn = Input.GetButton("Jump");
+        lastMousePos = mousePos;
         mousePos = Input.mousePosition;
         MousePosWorld = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
         MouseRay = cam.ScreenPointToRay(mousePos);
-        
+        //Debug.DrawRay(MouseRay.origin, MouseRay.direction * 10, Color.yellow); <- to track where the ray goes in scene view
 
 
     }
-
-  
+    public Vector3 mouseMovement()
+    {
+        return mousePos - lastMousePos;
+    }
 }
